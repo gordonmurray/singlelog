4cda478d029638f31ac60feb2adead5ad860f70b
Gordon Murray <gordon@gordonmurray.com>
2022-10-18 21:52:24 +0100

    bucket versioning and encryption

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -10,3 +10,21 @@ resource "aws_s3_bucket_acl" "logs_acl" {
   bucket = aws_s3_bucket.logs.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_versioning" "logs_versioning" {
+  bucket = aws_s3_bucket.logs.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs_encryption" {
+  bucket = aws_s3_bucket.logs.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
