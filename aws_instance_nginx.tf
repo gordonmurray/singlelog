52422499afa9655d23fe8c795dff613c005a2502
Gordon Murray <gordon@gordonmurray.ie>
2026-06-24 18:33:37 +0100

    Drop termination protection on the nginx instance
    It's a disposable demo box, and disable_api_termination blocked a clean
    terraform destroy.

--- aws_instance_nginx.tf
+++ aws_instance_nginx.tf
@@ -10,13 +10,12 @@ data "aws_ami" "nginx" {
 }
 
 resource "aws_instance" "nginx" {
-  ami                     = data.aws_ami.nginx.id
-  instance_type           = "t4g.micro"
-  key_name                = aws_key_pair.key.key_name
-  subnet_id               = var.subnet_id
-  vpc_security_group_ids  = [aws_security_group.nginx.id]
-  disable_api_termination = true
-  iam_instance_profile    = aws_iam_instance_profile.singlelog_profile.name
+  ami                    = data.aws_ami.nginx.id
+  instance_type          = "t4g.micro"
+  key_name               = aws_key_pair.key.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.nginx.id]
+  iam_instance_profile   = aws_iam_instance_profile.singlelog_profile.name
 
   root_block_device {
     delete_on_termination = true
